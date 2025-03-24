d6d9dda8573c70c7fb4980142acbe84ddfc5505a
imddc <easyhoo2426@163.com>
2025-03-24 12:00:00 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,10 @@ import HelloWorld from './components/HelloWorld.vue'
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    hh
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+
+      this is a sign
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
